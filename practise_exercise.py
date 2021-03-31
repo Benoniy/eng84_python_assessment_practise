2cbe651173ae51cd07f2bea547168306fc8d5c1d
Ben <benranson5@gmail.com>
2021-03-31 10:38:10 +0100

    2 practise questions

---
+++ practise_exercise.py
@@ -0,0 +1,18 @@
+# Create a function called greeting that takes a string argument
+# and return hello string
+
+def greeting(name):
+    return "Hello " + name
+
+
+# Test of greeting
+print(greeting("Ben"))
+
+
+# Declare a list of numbers from 1-9, iterate through the list and print each number
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for number in numbers:
+    print(number)
+
+
